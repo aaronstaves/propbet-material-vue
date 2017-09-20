6ed66f364c9896124c0662e15cc15bc78881d84a
Aaron Staves <aaron@aarons.io>
2017-09-19 23:25:46 -0500

    Contest list setup, not great, but works for now

--- src/components/Contest/Contests.vue
+++ src/components/Contest/Contests.vue
@@ -1,29 +1,50 @@
 <template>
   <v-container>
     <v-layout row wrap>
-      <v-flex xs12>
-        <v-card class="info">
+      <v-flex xs12 v-for="n in [1,2,3,4,5]" :key="n" mt-3 >
+        <v-card hover class="info">
           <v-container fluid>
-            <v-layout row>
-              <v-flex xs 4>
-                <v-card-title primary-title>Contest 1</v-card-title>
+
+            <v-layout row wrap>
+              <!-- title pane -->
+              <v-flex d-flex xs12 sm12 md4 class="text-xs-center white--text">
+                <v-icon dark x-large class="hidden-sm-and-down" style="font-size: 5em" >play_circle_outline</v-icon>
+                <h4 style="margin: auto">Week 2 Contest</h4>
+              </v-flex>
+
+              <!-- stats pane -->
+              <v-flex d-flex xs12 sm12 md4 class="text-xs-center white--text">
+                <v-layout row wrap>
+                  <v-flex xs12 style="margin: auto">
+                    <h2>10</h2>
+                    Participants
+                  </v-flex>
+                </v-layout>
+              </v-flex>
+
+              <!-- time pane -->
+              <v-flex d-flex xs12 sm12 md4 class="text-xs-center white--text">
+                <v-layout row wrap>
+                  <v-flex xs12>
+                    <h2>12 Hours</h2>
+                    remaining
+                  </v-flex>
+                </v-layout>
+              </v-flex>
+              <!-- action buttons -->
+              <v-flex xs12 mt-2>
                 <v-card-actions>
-                  <v-btn>Join</v-btn>
+                  <v-btn class="accent">Join</v-btn>
+                  <v-spacer class="hidden-md-and-up"></v-spacer>
+                  <v-btn class="accent">Edit</v-btn>
                 </v-card-actions>
-              </v-flex>
-              <v-flex xs6>
-                <div>Start Time: 2017-09-09</div>
-                <div>End Time: 2017-09-09</div>
-                <div>Bets 15</div>
-              </v-flex>
-              <v-flex xs2>
-                10 joined
               </v-flex>
             </v-layout>
           </v-container>
-        </v-card>
 
+        </v-card>
       </v-flex>
+
     </v-layout>
   </v-container>
 </template>
