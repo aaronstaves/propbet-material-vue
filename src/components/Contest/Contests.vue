ca102e04f96d287f64ad02668001d1c03d23619a
Aaron Staves <aaron@aarons.io>
2017-09-21 22:23:32 -0500

    Tweaks to style, simpler contest row design... for now
    kiss

--- src/components/Contest/Contests.vue
+++ src/components/Contest/Contests.vue
@@ -1,72 +1,46 @@
 <template>
   <v-container>
     <v-layout row wrap>
-      <v-flex xs12 v-for="n in [1,2,3,4,5]" :key="n" mt-3 >
-        <v-card hover class="info">
-          <v-card-title class="contest-title title accent">Week {{ n }} contest</v-card-title>
+      <v-flex xs12 v-for="contest in contests" :key="contest.id" mt-3 >
+        <v-card hover class="secondary">
+          <v-card-title class="contest-title title accent">
+            {{contest.name}}
+            <v-spacer></v-spacer>
+            {{contest.start}}
+          </v-card-title>
           <v-container fluid>
 
             <v-layout row wrap class="white--text">
-              <!-- Leaders -->
-              <v-flex d-flex xs12 sm12 md4>
-                <v-layout row wrap>
-                  <v-flex xs12 class="text-xs-center">
-                    <h6>Leaders</h6>
-                  </v-flex>
-                  <v-flex xs12 v-for="m in [3,2,1]" :key="m" mt-3>
-                    <v-layout row wrap>
-                      <v-flex xs12 class="text-xs-center">
-                        <v-layout row wrap>
-
-                          <!-- avatar -->
-                          <v-flex xs4>
-                            <div>
-                              <img class="avatar" src="http://68.media.tumblr.com/d467821e248f7a959b501ca90f17f2cb/tumblr_n8d8crh12o1rtbl5vo1_400.gif"> 
-                            </div>
-                          </v-flex>
-
-                          <!-- name -->
-                          <v-flex xs4>
-                            Leader 1
-                          </v-flex>
-
-                          <!-- points -->
-                          <v-flex xs4 class="accent--text">
-                            {{m * 5}} points
-                          </v-flex>
-                        </v-layout>
-                      </v-flex>
-                    </v-layout>
-                  </v-flex>
-                </v-layout>
-              </v-flex>
-
-              <!-- stats pane -->
-              <v-flex d-flex xs12 sm12 md4 class="text-xs-center">
+              <!-- participants pane -->
+              <v-flex d-flex xs6 sm6 md4 class="text-xs-center">
                 <v-layout row wrap>
                   <v-flex xs12 style="margin: auto">
-                    <h2>10</h2>
-                    Participants
+                    {{ contest.participants.length }} Participants
+                    <v-icon right dark>group</v-icon> 
                   </v-flex>
                 </v-layout>
               </v-flex>
 
-              <!-- time pane -->
-              <v-flex d-flex xs12 sm12 md4 class="text-xs-center">
+              <!-- bets pane -->
+              <v-flex d-flex xs6 sm6 md4 class="text-xs-center">
                 <v-layout row wrap>
                   <v-flex xs12 style="margin: auto">
-                    <h2>12 Hours</h2>
-                    remaining
+                    {{ contest.bets.length }} Bets
+                    <v-icon right dark>import_contacts</v-icon>
                   </v-flex>
                 </v-layout>
               </v-flex>
-              <!-- action buttons -->
-              <v-flex xs12 mt-2>
-                <v-card-actions style="font-size: 5em">
-                  <v-btn flat large class="accent--text">Join</v-btn>
-                  <v-spacer class="hidden-md-and-up"></v-spacer>
-                  <v-btn flat large class="accent--text">Edit</v-btn>
-                </v-card-actions>
+              
+              <!-- actions pane -->
+              <v-flex d-flex xs12 sm12 md4 class="text-xs-center">
+                <v-layout row wrap>
+                  <v-flex xs12>
+                    <v-btn flat class="yellow--text">
+                      Join
+                      <v-icon right dark class="yellow--text">play_circle_outline</v-icon>
+                    </v-btn>
+                  </v-flex>
+                </v-layout>
               </v-flex>
             </v-layout>
           </v-container>
@@ -77,6 +51,42 @@
     </v-layout>
   </v-container>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      contests: [],
+    };
+  },
+  created() {
+    const newContests = [];
+
+    newContests.push({
+      id: 1,
+      name: 'Contest Week 1',
+      start: new Date(),
+      participants: [1, 2, 3, 4, 5, 6],
+      bets: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
+    });
+    newContests.push({
+      id: 2,
+      name: 'Contest Week 2',
+      start: new Date(),
+      participants: [1, 2, 3, 4, 5, 6],
+      bets: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
+    });
+    newContests.push({
+      id: 3,
+      name: 'Contest Week 3',
+      start: new Date(),
+      participants: [1, 2, 3, 4, 5, 6],
+      bets: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
+    });
+    this.contests = newContests;
+  },
+};
+</script>
 
 <<style scoped>
 .avatar {
