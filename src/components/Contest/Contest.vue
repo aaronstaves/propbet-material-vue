84499a8b52c0b2e0679a07c1b544f2efb46fe3dd
Aaron Staves <aaron@aarons.io>
2017-09-25 22:07:37 -0500

    Update dummy store, build out Contest vuew page a bit more
    trying to use data tables, not sure if that's easier or if it'll be
    better to use vcards and style myself
    `

--- src/components/Contest/Contest.vue
+++ src/components/Contest/Contest.vue
@@ -1,45 +1,133 @@
 <template>
   <v-container>
     <v-layout row wrap>
-      <v-flex xs12>
-        <v-card hover class="secondary">
-          <v-card-title class="title accent">
-            {{ contest.name }}
-            <v-spacer></v-spacer>
-            {{ contest.date }}
-          </v-card-title>
+      <v-flex xs12 mt-3 >
+        <v-card hover class="accent">
 
-          <!-- start lower layout -->
-          <v-layout row wrap>
+          <v-container fluid>
+            <v-layout row wrap>
+              <!-- title pane -->
+              <v-flex d-flex xs12 class="mb-3">
+                <v-layout row wrap>
+                  <v-flex xs12 class="text-xs-center text-sm-left contest-title">
+                    <v-icon x-large class="hidden-xs-only">person_pin</v-icon>
+                    {{ contest.name }}
+                  </v-flex>
+                  <v-flex xs12 class="text-xs-center text-sm-left">
+                    Created by astaves  
+                  </v-flex>
+                </v-layout>
+              </v-flex>
 
-            <!-- participants pane -->
-            <v-flex xs6>
-              <v-card class="secondary">
-                <v-card-title class="title accent--text">Participants</v-card-title>
-                <v-flex v-for="user in contest.participants" :key="user" xs12>
-                  {{ user }}
-                </v-flex>
-              </v-card>
-            </v-flex>
-            
-            <!-- bets pane -->
-            <v-flex xs6>
-              <v-card class="secondary">
-                <v-card-title class="title accent--text">Bets</v-card-title>
-                <v-flex v-for="bet in contest.bets" :key="bet" xs12>
-                  {{ bet }}
-                </v-flex>
-              </v-card>
-            </v-flex>
-          </v-layout>
+              <!-- place pane -->
+              <v-flex d-flex xs4 md3 class="header-data text-xs-center">
+                <v-layout row wrap>
+                  <v-flex xs12>
+                    <strong class="primary--text">3rd</strong> / {{ contest.participants.length }}
+                  </v-flex>
+                  <v-flex xs12 class="header-caption">
+                    Place
+                  </v-flex>
+
+                </v-layout>
+              </v-flex>
+
+              <!-- bets pane -->
+              <v-flex d-flex xs4 md3 class="header-data text-xs-center">
+                <v-layout row wrap>
+                  <v-flex xs12>
+                    <strong class="success--text">6</strong> / {{ contest.bets.length }}
+                  </v-flex>
+                  <v-flex xs12 class="header-caption">
+                    Bets
+                  </v-flex>
+                </v-layout>
+              </v-flex>
+
+              <!-- unresolved pane -->
+              <v-flex d-flex xs4 md3 class="header-data text-xs-center">
+                <v-layout row wrap>
+                  <v-flex xs12>
+                    <strong>4</strong> / {{ contest.bets.length }}
+                  </v-flex>
+                  <v-flex xs12 class="header-caption">
+                    Unresolved
+                  </v-flex>
+                </v-layout>
+              </v-flex>
+
+              <!-- time remaining pane -->
+              <v-flex d-flex xs12 md3 class="header-data text-xs-center">
+                <v-layout row wrap>
+                  <v-flex xs12 class="hidden-sm-and-down">
+                      12:03 Remaining
+                  </v-flex>
+                 <v-flex xs12 class="header-caption hidden-md-and-up mt-3">
+                      12:03 Remaining
+                  </v-flex>
+                  <v-flex xs8 offset-xs2 md12>
+                      <v-progress-linear color-front="success" color-back="error" v-model="placePercent"></v-progress-linear>
+                  </v-flex>
+                </v-layout>
+              </v-flex>              
+            </v-layout>
+          </v-container>
+
         </v-card>
       </v-flex>
+
+
+       <v-flex xs12 mt-3 >
+        <v-card hover class="accent">
+          <v-container fluid>
+            <v-layout row wrap>
+              <!-- title pane -->
+              <v-flex d-flex xs12 class="mb-3">
+                <v-layout row wrap>
+
+                  <!-- leaderboard title -->
+                  <v-flex xs12 class="text-xs-center text-sm-left leaderboard-title mb-3">
+                    <v-icon x-large class="hidden-xs-only">list</v-icon>
+                    Leaderboard
+                  </v-flex>
+
+                  <v-flex>
+                    <v-data-table
+                        v-bind:headers="headers"
+                        :items="contest.participants"
+                        hide-actions
+                        class="elevation-1"
+                      >
+                      <template slot="items" scope="props">
+                        <td>{{ props.item.position }}</td>
+                        <td class="text-xs-right">{{ props.item.name }}</td>
+                        <td class="text-xs-right">{{ props.item.score }}</td>
+                      </template>
+                    </v-data-table>
+                  </v-flex>
+                </v-layout>
+              </v-flex>
+            </v-layout>
+          </v-container>
+        </v-card>
+       </v-flex>
     </v-layout>
   </v-container>
 </template>
 
+
 <script>
 export default {
+  data() {
+    return {
+      headers: [
+        { text: 'position', value: 'position' },
+        { text: 'user', value: 'name' },
+        { text: 'score', value: 'score' },
+      ],
+      placePercent: 50,
+    };
+  },
   props: ['id'],
   computed: {
     contest() {
@@ -48,3 +136,19 @@ export default {
   },
 };
 </script>
+
+<style scoped>
+.contest-title {
+  font-size: 2.5em;
+}
+.header-data {
+  font-size: 1.5em;
+}
+.header-caption {
+  font-size: 0.5em;
+}
+
+.leaderboard-title {
+  font-size: 1.5em;
+}
+</style>
--- src/store/index.js
+++ src/store/index.js
@@ -3,13 +3,41 @@ import Vuex from 'vuex';
 
 Vue.use(Vuex);
 
+const participants = [
+  {
+    name: 'astaves',
+    position: 4,
+    score: 5,
+  },
+  {
+    name: 'logan11',
+    position: 1,
+    score: 9,
+  },
+  {
+    name: 'ss',
+    position: 3,
+    score: 6,
+  },
+  {
+    name: 'kingthursday',
+    position: 2,
+    score: 7,
+  },
+  {
+    name: 'deepcrow',
+    position: 5,
+    score: 2,
+  },
+];
+
 export const store = new Vuex.Store({
   state: {
     loadedContests: [{
       id: 1,
       name: 'Contest Week 1',
       start: new Date(),
-      participants: [1, 2, 3, 4, 5, 6],
+      participants,
       bets: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
     },
     {
